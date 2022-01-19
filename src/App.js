11d23f75d61e6a11030ff568518a1d958e51fd9b
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2022-01-19 16:51:16 +0200

    added packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,81 @@
-function App() {
-  return <>hello</>;
-}
+import React, { Component } from "react";
+import shortid from "shortid";
 
+import contactList from "./phonelist.json";
+
+import ContactList from "./components/ContactList";
+import ContactForm from "./components/ContactForm";
+import ContactFilter from "./components/ContactFilter";
+
+class App extends Component {
+  state = {
+    contacts: contactList,
+    filter: "",
+  };
+
+  addContact = ({ name, number }) => {
+    if (this.isInContacts(name)) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+
+    this.setState((prevState) => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  isInContacts = (name) => {
+    name = name.toLowerCase();
+    const isFoundName = this.state.contacts.find(
+      (contact) => contact.name.toLowerCase() === name
+    );
+    if (isFoundName) {
+      alert(`${name} was found`);
+      return;
+    }
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
+
+  //фильтр,принятие ивента
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  // метод фильтрации массива
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <ContactFilter onChange={this.changeFilter} value={filter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
 export default App;
